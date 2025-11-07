4e0a81f9b19b584ab81fec42a272879c2cb6f53c
Dr. Cleve <76855369+David-Orangemoon@users.noreply.github.com>
2025-11-06 20:42:24 -0500

    Add effective

--- extensions/effective/extension.js
+++ extensions/effective/extension.js
@@ -19,13 +19,21 @@
 uniform mat4 u_projectionMatrix;
 uniform mat4 u_modelMatrix;
 
+uniform float u_skewX;
+uniform float u_skewY;
+
 attribute vec2 a_texCoord;
 attribute vec2 a_position;
 
 varying vec2 v_texCoord;
 
 void main() {
-	gl_Position = u_projectionMatrix * u_modelMatrix * vec4(a_position, 0, 1);
+	gl_Position = u_projectionMatrix * u_modelMatrix * (mat4(
+        1, u_skewY / 100.0, 0, 0,
+        u_skewX / 100.0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    ) * vec4(a_position, 0, 1));
 	v_texCoord = a_texCoord;
 }`;
 
@@ -40,9 +48,25 @@ uniform float u_pixelate;
 uniform vec2 u_skinSize;
 uniform float u_mosaic;
 uniform float u_ghost;
+
+uniform float u_waveAmpX;
+uniform float u_waveAmpY;
+uniform float u_waveSizeX;
+uniform float u_waveSizeY;
+uniform float u_waveProgress;
+
+uniform float u_saturation;
+uniform float u_sepia;
+
 uniform float u_red;
 uniform float u_green;
 uniform float u_blue;
+
+uniform float u_unfocus;
+uniform lowp int u_blur;
+
+uniform bool u_oldColor;
+uniform bool u_repeat;
 
 varying vec2 v_texCoord;
 
@@ -56,7 +80,6 @@ const float epsilon = 1e-3;
 // http://www.chilliant.com/rgb2hsv.html by Ian Taylor
 // Based in part on work by Sam Hocevar and Emil Persson
 // See also: https://en.wikipedia.org/wiki/HSL_and_HSV#Formal_derivation
-
 
 // Convert an RGB color to Hue, Saturation, and Value.
 // All components of input and output are expected to be in the [0,1] range.
@@ -114,6 +137,16 @@ void main()
 {
 	vec2 texcoord0 = v_texCoord;
 
+    if (u_waveAmpX != 0.0) {
+        texcoord0.x += sin(u_waveProgress + texcoord0.x * u_waveSizeX) * u_waveAmpX;
+    }
+
+    if (u_waveAmpY != 0.0) {
+        texcoord0.y += sin(u_waveProgress + texcoord0.y * u_waveSizeY) * u_waveAmpY;
+    }
+
+    if (!u_repeat && (texcoord0.x < 0.0 || texcoord0.y < 0.0 || texcoord0.x > 1.0 || texcoord0.y > 1.0)) { discard; }
+
     //Mosaic
 	texcoord0 = fract(u_mosaic * texcoord0);
     if (u_pixelate > 0.0)
@@ -154,6 +187,57 @@ void main()
 
 	gl_FragColor = texture2D(u_skin, texcoord0);
 
+    if (u_unfocus > 0.0) {
+        float blurDist =  u_unfocus / 1000.0;
+        for (int i = 0; i<4; i++) {
+            float curDist = blurDist * float(i);
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(0, curDist));
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(0, -curDist));
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(curDist, 0));
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(-curDist, 0));
+
+
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(curDist, curDist));
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(curDist, -curDist));
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(-curDist, -curDist));
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(-curDist, curDist));
+        }
+
+        gl_FragColor /= 33.0;
+    }
+
+    if (u_blur > 0) {
+        vec2 pixelTexelSize = 1.0 / u_skinSize;
+        float divider = 1.0;
+        float unfocused = 1.0;
+        if (u_unfocus > 0.0) {
+            unfocused = 1.0 / u_unfocus;
+        }
+
+        float dividerStep = 8.0 * unfocused;
+
+        for (int i = 0; i<128; i++) {
+            if (i >= u_blur) { break; }
+
+            vec2 blurStep = pixelTexelSize * float(i);
+
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(0, blurStep.y)) * unfocused;
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(0, -blurStep.y)) * unfocused;
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(blurStep.x, 0)) * unfocused;
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(-blurStep.x, 0)) * unfocused;
+
+
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(blurStep.x, blurStep.y)) * unfocused;
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(blurStep.x, -blurStep.y)) * unfocused;
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(-blurStep.x, -blurStep.y)) * unfocused;
+            gl_FragColor += texture2D(u_skin, texcoord0 + vec2(-blurStep.x, blurStep.y)) * unfocused;
+
+            divider += dividerStep;
+        }
+
+        gl_FragColor /= divider;
+    }
+    
 	// Divide premultiplied alpha values for proper color processing
 	// Add epsilon to avoid dividing by 0 for fully transparent pixels
 	gl_FragColor.rgb = clamp(gl_FragColor.rgb / (gl_FragColor.a + epsilon), 0.0, 1.0);
@@ -167,10 +251,12 @@ void main()
 
 		// this code forces grayscale values to be slightly saturated
 		// so that some slight change of hue will be visible
-		const float minLightness = 0.11 / 2.0;
-		const float minSaturation = 0.09;
-		if (hsv.z < minLightness) hsv = vec3(0.0, 1.0, minLightness);
-		else if (hsv.y < minSaturation) hsv = vec3(0.0, minSaturation, hsv.z);
+        if (u_oldColor) {
+            const float minLightness = 0.11 / 2.0;
+            const float minSaturation = 0.09;
+            if (hsv.z < minLightness) hsv = vec3(0.0, 1.0, minLightness);
+            else if (hsv.y < minSaturation) hsv = vec3(0.0, minSaturation, hsv.z);
+        }
 
 		hsv.x = mod(hsv.x + u_color, 1.0);
 		if (hsv.x < 0.0) hsv.x += 1.0;
@@ -178,8 +264,30 @@ void main()
 		gl_FragColor.rgb = convertHSV2RGB(hsv);
 	}
 
+    //Saturation
+    if (u_saturation != 0.0) {
+		vec3 hsv = convertRGB2HSV(gl_FragColor.xyz);
+        hsv.y += u_saturation / 100.0;
+		gl_FragColor.rgb = convertHSV2RGB(hsv);
+    }
+
     //Brightness
-    gl_FragColor.rgb = clamp(gl_FragColor.rgb + vec3(u_brightness), vec3(0), vec3(1));
+    if (u_oldColor) {
+        gl_FragColor.rgb = clamp(gl_FragColor.rgb + vec3(u_brightness), vec3(0), vec3(1));
+    }
+    else {
+		vec3 hsv = convertRGB2HSV(gl_FragColor.xyz);
+        hsv.z += u_brightness;
+		gl_FragColor.rgb = convertHSV2RGB(hsv);
+    }
+
+    if (u_sepia >= 0.0) {
+        float brightest = gl_FragColor.x;
+        if (gl_FragColor.y > brightest) { brightest = gl_FragColor.y; }
+        if (gl_FragColor.z > brightest) { brightest = gl_FragColor.z; }
+
+        gl_FragColor.xyz = mix(gl_FragColor.xyz, vec3(brightest, brightest * 0.75, brightest * 0.5), u_sepia / 100.0);
+    }
 
 	// Re-multiply color values
 	gl_FragColor.rgb *= gl_FragColor.a + epsilon;
@@ -200,6 +308,47 @@ void main()
         u_ghost: "ghost",
     }
 
+    defaultValues = {
+        u_saturation: 0,
+        u_red: 100,
+        u_green: 100,
+        u_blue: 100,
+
+        u_waveAmpX: 0,
+        u_waveAmpY: 0,
+        u_waveSizeX: 3.1415962,
+        u_waveSizeY: 3.1415962,
+        u_waveProgress: 0,
+
+        u_blur: 0,
+        u_unfocus: 0,
+        
+        u_skewX: 0,
+        u_skewY: 0,
+
+        u_oldColor: true,
+        u_repeat: false
+    }
+
+    maxRanges = {
+        u_saturation: [-100, 100],
+        u_red: [0, 100],
+        u_green: [0, 100],
+        u_blue: [0, 100],
+        
+        u_waveAmpX: [-1, 1],
+        u_waveAmpY: [-1, 1],
+        u_waveSizeX: [-6.28318, 6.28318],
+        u_waveSizeY: [-6.28318, 6.28318],
+        u_waveProgress: [-Infinity, Infinity],
+        
+        u_blur: [0, 128],
+        u_unfocus: [0, 100],
+        
+        u_skewX: [-1, 1],
+        u_skewY: [-1, 1],
+    }
+
     constructor() {
         this.newShader = twgl.createProgramInfo(gl, [
             //Scoot this in
@@ -215,14 +364,39 @@ void main()
             } 
         }
 
-        vm.runtime.on("targetWasAdded", (clone) => {
-            console.log(clone);
+        runtime.on("targetWasCreated", (target) => {
+            if (target.isOriginal) {
+                renderer._allDrawables[target.drawableID]._uniforms = {
+                    ...renderer._allDrawables[target.drawableID]._uniforms,
+                    ...this.defaultValues
+                }
+            }
+            else {
+                renderer._allDrawables[target.drawableID]._uniforms = {...renderer._allDrawables[target.sprite.clones[0].drawableID]._uniforms};
+            }
         });
+
+        //Load default values
+        for (let targetID in runtime.targets) {
+            const target = runtime.targets[targetID];
+
+            renderer._allDrawables[target.drawableID]._uniforms = {...renderer._allDrawables[target.sprite.clones[0].drawableID]._uniforms};
+        }
     }
 
     getInfo() {
       return {
         blocks: [
+            {
+                blockType: Scratch.BlockType.COMMAND,
+                text: "change [variable] effect by [value]",
+                arguments: {
+                    variable: { menu: "effects", defaultValue: "u_color" },
+                    value: { type: Scratch.ArgumentType.NUMBER, defaultValue: 25 }
+                },
+                opcode: "changeNumberVar",
+                extensions: ["colours_looks"],
+            },
             {
                 blockType: Scratch.BlockType.COMMAND,
                 text: "set [variable] effect to [value]",
@@ -231,6 +405,17 @@ void main()
                     value: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 }
                 },
                 opcode: "setNumberVar",
+                extensions: ["colours_looks"],
+            },
+            "---",
+            {
+                blockType: Scratch.BlockType.COMMAND,
+                text: "set [variable] effect to [value]",
+                arguments: {
+                    variable: { menu: "options", defaultValue: "u_oldColor" },
+                    value: { menu: "boolean", defaultValue: "true" }
+                },
+                opcode: "setOption",
                 extensions: ["colours_looks"],
             }
         ],
@@ -241,6 +426,30 @@ void main()
                     {
                         text: "color",
                         value: "u_color"
+                    },
+                    {
+                        text: "saturation",
+                        value: "u_saturation"
+                    },
+                    {
+                        text: "brightness",
+                        value: "u_brightness"
+                    },
+                    {
+                        text: "sepia",
+                        value: "u_sepia"
+                    },
+                    {
+                        text: "red amount",
+                        value: "u_red"
+                    },
+                    {
+                        text: "green amount",
+                        value: "u_green"
+                    },
+                    {
+                        text: "blue amount",
+                        value: "u_blue"
                     },
                     {
                         text: "fisheye",
@@ -259,25 +468,52 @@ void main()
                         value: "u_mosaic"
                     },
                     {
-                        text: "brightness",
-                        value: "u_brightness"
+                        text: "wave amplitude x",
+                        value: "u_waveAmpX"
+                    },
+                    {
+                        text: "wave amplitude y",
+                        value: "u_waveAmpY"
+                    },
+                    {
+                        text: "wave size x",
+                        value: "u_waveSizeX"
+                    },
+                    {
+                        text: "wave size y",
+                        value: "u_waveSizeY"
+                    },
+                    {
+                        text: "wave offset",
+                        value: "u_waveProgress"
                     },
                     {
                         text: "ghost",
                         value: "u_ghost"
                     },
                     {
-                        text: "red amount",
-                        value: "u_red"
+                        text: "blur",
+                        value: "u_blur"
                     },
                     {
-                        text: "green amount",
-                        value: "u_green"
+                        text: "unfocus",
+                        value: "u_unfocus"
                     },
                     {
-                        text: "blue amount",
-                        value: "u_blue"
+                        text: "skew x",
+                        value: "u_skewX"
+                    },
+                    {
+                        text: "skew y",
+                        value: "u_skewY"
                     }
+                ],
+                acceptReporters: true
+            },
+            options: {
+                items: [
+                    { text: "use old color functions", value: "u_oldColor" },
+                    { text: "repeat costumes", value: "u_repeat" }
                 ],
                 acceptReporters: true
             },
@@ -297,11 +533,40 @@ void main()
     }
 
     setNumberVar({ variable, value }, { target }) {
-        if (this.defaultEffects[variable]) target.setEffect(this.defaultEffects[variable], Scratch.Cast.toNumber(value));
+        const defaultEffect = this.defaultEffects[variable];
+        if (defaultEffect) target.setEffect(defaultEffect, Scratch.Cast.toNumber(value));
+        else if (typeof this.defaultValues[variable] == "boolean") return;
         else {
-            renderer._allDrawables[target.drawableID]._uniforms[variable] = Scratch.Cast.toNumber(value);
+            const drawable = renderer._allDrawables[target.drawableID];
+            drawable._uniforms[variable] = Scratch.Cast.toNumber(value);
+
+            //Clamp to limits
+            if (runtime.runtimeOptions.miscLimits) drawable._uniforms[variable] = Math.min(Math.max(drawable._uniforms[variable], this.maxRanges[variable][0]), this.maxRanges[variable][1]);
+
             vm.renderer.dirty = true;
         }
+    }
+
+    changeNumberVar({ variable, value }, { target }) {
+        const defaultEffect = this.defaultEffects[variable];
+        if (defaultEffect) target.setEffect(defaultEffect, target.effects[defaultEffect] + Scratch.Cast.toNumber(value));
+        else if (typeof this.defaultValues[variable] == "boolean") return;
+        else {
+            const drawable = renderer._allDrawables[target.drawableID];
+            drawable._uniforms[variable] += Scratch.Cast.toNumber(value);
+
+            //Clamp to limits
+            if (runtime.runtimeOptions.miscLimits) drawable._uniforms[variable] = Math.min(Math.max(drawable._uniforms[variable], this.maxRanges[variable][0]), this.maxRanges[variable][1]);
+            vm.renderer.dirty = true;
+        }
+    }
+
+    setOption({ variable, value }, { target }) {
+        if (this.defaultEffects[variable]) return;
+        if (typeof this.defaultValues[variable] != "boolean") return;
+
+        renderer._allDrawables[target.drawableID]._uniforms[variable] = Scratch.Cast.toBoolean(value);
+        vm.renderer.dirty = true;
     }
   }
 
